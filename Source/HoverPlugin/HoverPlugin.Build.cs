3879d0d3bea777e561be997609796f206a9fb7f7
Gerke Max Preussner <github@gerke-preussner.de>
2017-08-30 16:39:15 -0400

    Fixed indentation.

--- Source/HoverPlugin/HoverPlugin.Build.cs
+++ Source/HoverPlugin/HoverPlugin.Build.cs
@@ -6,54 +6,46 @@ public class HoverPlugin : ModuleRules
 {
 	public HoverPlugin(ReadOnlyTargetRules Target) : base(Target)
 	{
-        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-        PublicIncludePaths.AddRange(
+		PublicIncludePaths.AddRange(
 			new string[] {
 				"HoverPlugin/Public"
 				
 				// ... add public include paths required here ...
-			}
-		);
-				
-		
+			});
+
+
 		PrivateIncludePaths.AddRange(
 			new string[] {
 				"HoverPlugin/Private",
 				
 				// ... add other private include paths required here ...
-    		}
-		);
-			
-		
+    		});
+
+
 		PublicDependencyModuleNames.AddRange(
-			new string[]
-			{
+			new string[] {
 				"Core",
-                "CoreUObject",
-                "Engine",
-                "InputCore",
+				"CoreUObject",
+				"Engine",
+				"InputCore",
 				
 				// ... add other public dependencies that you statically link with here ...
-			}
-		);
-			
-		
+			});
+
+
 		PrivateDependencyModuleNames.AddRange(
-			new string[]
-			{
+			new string[] {
 				"Slate", "SlateCore"
 				// ... add private dependencies that you statically link with here ...	
-			}
-		);
-		
-		
+			});
+
+
 		DynamicallyLoadedModuleNames.AddRange(
-			new string[]
-			{
+			new string[] {
 				
 				// ... add any modules that your module loads dynamically here ...
-			}
-		);
+			});
 	}
 }
